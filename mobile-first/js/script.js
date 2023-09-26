3ea2a972a99c08d13c3a9ccd1192cb52fd9d4860
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2023-09-26 12:48:45 +0600

    Mobile First Responsive

---
+++ mobile-first/js/script.js
@@ -0,0 +1,4 @@
+const hamburger = document.querySelector('.hamburger');
+const navlist = document.querySelector('.nav-list');
+
+hamburger.addEventListener('click', () => { navlist.classList.toggle('show'); });
